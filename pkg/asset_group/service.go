1f2da8947daf0a942b81eab8a4125c4e8ba59d32
Leslie Aula <aulaleslie@gmail.com>
2023-07-17 03:54:43 +0800

    add insert, update and delete. add todo for get

--- pkg/asset_group/service.go
+++ pkg/asset_group/service.go
@@ -2,9 +2,12 @@ package asset_group
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
 
 	"github.com/aulaleslie/asset-service/pkg/asset_group/pb"
 	"github.com/aulaleslie/asset-service/pkg/db"
+	"github.com/aulaleslie/asset-service/pkg/models"
 )
 
 type Server struct {
@@ -13,100 +16,107 @@ type Server struct {
 }
 
 func (s *Server) Create(ctx context.Context, in *pb.CreateUpdateRequest) (*pb.CUDResponse, error) {
-	return &pb.CUDResponse{}, nil
+	var assetsGroup models.AssetsGroup
+
+	agrBusinessGroup, err := json.Marshal(in.AgrBusinessGroup)
+	if err != nil {
+		return nil, err
+	}
+	assetsGroup.AgrBusinessGroup = string(agrBusinessGroup)
+
+	if in.AgrGroupName == "" {
+		return &pb.CUDResponse{
+			Status: false,
+			Data: &pb.Data{
+				Code:    422,
+				Message: "Group Name is required",
+			},
+		}, nil
+	}
+	assetsGroup.AgrGroupName = in.AgrGroupName
+
+	if err = s.H.DB.Create(&assetsGroup).Error; err != nil {
+		return &pb.CUDResponse{
+			Status: false,
+			Data: &pb.Data{
+				Code:    http.StatusInternalServerError,
+				Message: err.Error(),
+			},
+		}, nil
+	}
+
+	return &pb.CUDResponse{
+		Status: true,
+		Data: &pb.Data{
+			Code: http.StatusCreated,
+		},
+	}, nil
 }
 
 func (s *Server) Update(ctx context.Context, in *pb.CreateUpdateRequest) (*pb.CUDResponse, error) {
-	return &pb.CUDResponse{}, nil
+	var assetsGroup models.AssetsGroup
+
+	agrBusinessGroup, err := json.Marshal(in.AgrBusinessGroup)
+	if err != nil {
+		return nil, err
+	}
+	assetsGroup.AgrBusinessGroup = string(agrBusinessGroup)
+
+	if in.AgrGroupName == "" {
+		return &pb.CUDResponse{}, nil
+	}
+
+	assetsGroup.AgrGroupName = in.AgrGroupName
+
+	if res := s.H.DB.Save(&assetsGroup); res.Error != nil {
+		return &pb.CUDResponse{
+			Status: false,
+			Data: &pb.Data{
+				Code: http.StatusConflict,
+			},
+		}, nil
+	}
+
+	return &pb.CUDResponse{
+		Status: true,
+	}, nil
 }
 
 func (s *Server) Delete(ctx context.Context, in *pb.DeleteRequest) (*pb.CUDResponse, error) {
-	return &pb.CUDResponse{}, nil
+	var assetGroup models.AssetsGroup
+
+	if result := s.H.DB.Find(&assetGroup, in.Id); result.Error != nil {
+		return &pb.CUDResponse{
+			Status: false,
+			Data: &pb.Data{
+				Name:    "Not Found",
+				Code:    http.StatusNotFound,
+				Message: "Page not found",
+			},
+		}, nil
+	}
+
+	if result := s.H.DB.Delete(&assetGroup); result.Error != nil {
+		return &pb.CUDResponse{
+			Status: false,
+			Data: &pb.Data{
+				Name:    "Unauthorized",
+				Message: "Your request was made with invalid credentials.",
+				Code:    401,
+			},
+		}, nil
+	}
+
+	return &pb.CUDResponse{
+		Status: true,
+		Data: &pb.Data{
+			Code:    200,
+			Message: "Group Successfully Deleted",
+		},
+	}, nil
 }
 
 func (s *Server) Read(ctx context.Context, in *pb.ReadRequest) (*pb.ReadResponse, error) {
+	// TODO need to confirm Omar about BusinessGroup, SubGroup, AssetType, Value (lack information)
 	return &pb.ReadResponse{}, nil
 }
-
-// func (s *Server) CreateAsset(ctx context.Context, req *pb.CreateAssetRequest) (*pb.CreateAssetResponse, error) {
-// 	var asset models.Asset
-
-// 	asset.Name = req.Name
-// 	asset.Stock = req.Stock
-// 	asset.Price = req.Price
-
-// 	if result := s.H.DB.Create(&asset); result.Error != nil {
-// 		return &pb.CreateAssetResponse{
-// 			Status: http.StatusConflict,
-// 			Error:  result.Error.Error(),
-// 		}, nil
-// 	}
-
-// 	return &pb.CreateAssetResponse{
-// 		Status: http.StatusCreated,
-// 		Id:     asset.Id,
-// 	}, nil
-// }
-
-// func (s *Server) FindOne(ctx context.Context, req *pb.FindOneRequest) (*pb.FindOneResponse, error) {
-// 	var asset models.Asset
-
-// 	if result := s.H.DB.First(&asset, asset.Id); result.Error != nil {
-// 		return &pb.FindOneResponse{
-// 			Status: http.StatusNotFound,
-// 			Error:  result.Error.Error(),
-// 		}, nil
-// 	}
-
-// 	data := &pb.FindOneData{
-// 		Id:    asset.Id,
-// 		Name:  asset.Name,
-// 		Stock: asset.Stock,
-// 		Price: asset.Price,
-// 	}
-
-// 	return &pb.FindOneResponse{
-// 		Status: http.StatusOK,
-// 		Data:   data,
-// 	}, nil
-// }
-
-// func (s *Server) DecreaseStock(ctx context.Context, req *pb.DecreaseStockRequest) (*pb.DecreaseStockResponse, error) {
-// 	var asset models.Asset
-
-// 	if result := s.H.DB.First(&asset, req.Id); result.Error != nil {
-// 		return &pb.DecreaseStockResponse{
-// 			Status: http.StatusNotFound,
-// 			Error:  result.Error.Error(),
-// 		}, nil
-// 	}
-
-// 	if asset.Stock <= 0 {
-// 		return &pb.DecreaseStockResponse{
-// 			Status: http.StatusConflict,
-// 			Error:  "Stock too low",
-// 		}, nil
-// 	}
-
-// 	var log models.StockDecreaseLog
-
-// 	if result := s.H.DB.Where(&models.StockDecreaseLog{OrderId: req.OrderId}).First(&log); result.Error != nil {
-// 		return &pb.DecreaseStockResponse{
-// 			Status: http.StatusConflict,
-// 			Error:  "Stock already decrease",
-// 		}, nil
-// 	}
-
-// 	asset.Stock = asset.Stock - 1
-
-// 	s.H.DB.Save(&asset)
-
-// 	log.OrderId = req.OrderId
-// 	log.AssetRefer = asset.Id
-
-// 	s.H.DB.Create(&log)
-
-// 	return &pb.DecreaseStockResponse{
-// 		Status: http.StatusOK,
-// 	}, nil
-// }
--- pkg/asset_sub_group/service.go
+++ pkg/asset_sub_group/service.go
@@ -2,9 +2,12 @@ package asset_sub_group
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
 
 	"github.com/aulaleslie/asset-service/pkg/asset_sub_group/pb"
 	"github.com/aulaleslie/asset-service/pkg/db"
+	"github.com/aulaleslie/asset-service/pkg/models"
 )
 
 type Server struct {
@@ -13,17 +16,93 @@ type Server struct {
 }
 
 func (s *Server) Create(ctx context.Context, in *pb.CreateUpdateRequest) (*pb.CUDResponse, error) {
+	var assetSubGroup models.AssetSubGroup
+
+	asgParentGroup, err := json.Marshal(in.AsgParentGroup)
+	if err != nil {
+		return nil, err
+	}
+
+	assetSubGroup.AsgParentGroup = string(asgParentGroup)
+
+	if in.AsgName == "" {
+		return &pb.CUDResponse{
+			Status: false,
+			Data:   &pb.Data{Code: 422, Message: "Group Name is required"},
+		}, nil
+
+	}
+	assetSubGroup.AsgName = in.AsgName
+
+	if result := s.H.DB.Create(&assetSubGroup); result.Error != nil {
+		return &pb.CUDResponse{
+			Status: false,
+			Data:   &pb.Data{Code: http.StatusInternalServerError, Message: result.Error.Error()},
+		}, nil
+	}
 	return &pb.CUDResponse{}, nil
 }
 
 func (s *Server) Update(ctx context.Context, in *pb.CreateUpdateRequest) (*pb.CUDResponse, error) {
-	return &pb.CUDResponse{}, nil
+	var assetSubGroup models.AssetSubGroup
+
+	asgParentGroup, err := json.Marshal(&assetSubGroup)
+	if err != nil {
+		return nil, err
+	}
+	assetSubGroup.AsgParentGroup = string(asgParentGroup)
+
+	if result := s.H.DB.Find(&assetSubGroup, in.Id); result.Error != nil {
+		return &pb.CUDResponse{
+			Status: false,
+			Data:   &pb.Data{Code: http.StatusNotFound, Message: "id not found"},
+		}, nil
+	}
+
+	assetSubGroup.AsgName = in.AsgName
+	assetSubGroup.AsgParentGroup = string(asgParentGroup)
+
+	return &pb.CUDResponse{
+		Status: false,
+		Data:   &pb.Data{Code: http.StatusOK, Message: "Update success"},
+	}, nil
 }
 
 func (s *Server) Delete(ctx context.Context, in *pb.DeleteRequest) (*pb.CUDResponse, error) {
-	return &pb.CUDResponse{}, nil
+	var assetSubGroup models.AssetSubGroup
+
+	if result := s.H.DB.Find(&assetSubGroup, in.Id); result.Error != nil {
+		return &pb.CUDResponse{
+			Status: false,
+			Data: &pb.Data{
+				Name:    "Not Found",
+				Code:    http.StatusNotFound,
+				Message: "Page not found",
+			},
+		}, nil
+	}
+
+	if result := s.H.DB.Delete(&assetSubGroup); result.Error != nil {
+		return &pb.CUDResponse{
+			Status: false,
+			Data: &pb.Data{
+				Name:    "Unauthorized",
+				Message: "Your request was made with invalid credentials.",
+				Code:    401,
+			},
+		}, nil
+	}
+
+	return &pb.CUDResponse{
+		Status: true,
+		Data: &pb.Data{
+			Code:    200,
+			Message: "Group Successfully Deleted",
+		},
+	}, nil
 }
 
 func (s *Server) Read(ctx context.Context, in *pb.ReadRequest) (*pb.ReadResponse, error) {
+	// TODO create query. need information about AssetsGroup map query.
 	return &pb.ReadResponse{}, nil
 }
